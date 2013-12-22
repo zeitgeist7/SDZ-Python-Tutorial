8982d25b805476cdc1e06ce8ca7e526846419e8d
zeitgeist7 <zeitgeist7@gmail.com>
2013-12-22 23:44:48 +0100

    Add the files that were somewhat not staged

--- Partie 2/fonctions.py
+++ Partie 2/fonctions.py
@@ -1,5 +1,23 @@
 #!/usr/local/bin/py
 
+# Open and write to a file
 fichier = open("./testfile.txt", "w")
-fichier.write("This is a day that I have been looking forward to for two and a half years\n")
+fichier.write("This is a day that I have been looking forward to for two and a half years.\n")
 fichier.close()
+
+# Open the file and append some contents to it
+fichier = open("testfile.txt", "a")
+fichier.write("Every once in a while, a revolutionary product comes along that changes everything.\n")
+fichier.close()
+
+# Use a for loop to write the line numbers in the file (boring)
+fichier1 = open("colle.txt", "w")
+for index in range(100) :
+		fichier1.write("Line number " + str(index + 1) + ".\n")
+fichier1.close()
+
+# Write a list to a file
+fruitList = ['oranges\n', 'goyaves\n', 'goyaves de chine\n', 'abricot\n', 'jamblons\n', 'papayes\n']
+fruits = open("./fruits.txt", "w")
+fruits.writelines(fruitList)
+fruits.close()
