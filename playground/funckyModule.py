7c11a7c0b1ba5cf00f225bafa795ae8eea632203
zeitgeist7 <zeitgeist7@gmail.com>
2013-12-31 06:10:55 +0100

    Restructure files
    Create a directory to place the playgroup related tasks
    Moves playground.py in playground directory
    Touch a module (funky is wrongly written and I do not care)
    
    #organizes_files

---
+++ playground/funckyModule.py
@@ -0,0 +1,9 @@
+def printHello():
+	print("Hello")
+
+def printWorld():
+	print("World")
+
+def steveSaid():
+	print('This is a day that I have been looking forward to for two and a half years. Every once in a while a revolutionary product comes along that changes everything.')
+
