a9544d5fdaa3f9207303285cb8eb5ac3d2487e6d
zeitgeist7 <zeitgeist7@gmail.com>
2014-01-02 00:55:23 +0100

    Conditional execution\

--- playground/funckyModule.py
+++ playground/funckyModule.py
@@ -7,3 +7,10 @@ def printWorld():
 def steveSaid():
 	print('This is a day that I have been looking forward to for two and a half years. Every once in a while a revolutionary product comes along that changes everything.')
 
+if __name__ == '__main__':
+	print('We are printing some stuff as we are executing the module itself as the main function')
+	printHello()
+	printWorld()
+	steveSaid()
+
+
