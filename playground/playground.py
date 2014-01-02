db14ec68c94c39ddd84393953f1af8dec0737f96
zeitgeist7 <zeitgeist7@gmail.com>
2014-01-02 04:54:37 +0100

    Add comment and documention for the function
    Archive the super cool recursive factorial fucntion
    Add description of the factorial function
    
    #documentation_yo

--- playground/playground.py
+++ playground/playground.py
@@ -6,15 +6,21 @@ So far, Python seems a bit boring. But this tutorial seems promising.
 Do not expect to be amazed if you have opened this file by curiousity!!!
 '''
 
+'''
 def factorial(number) :
+	"""
+	Just a normal recursive function for the famous factorial function
+	I have no idea whether that is very efficient since in JS it is not recommended to use recusion
+	number : number whose factorial is to be calculated
+	return : number!
+	"""
 	if number <= 1:
 		return 1
 	else :
 		return number * factorial(number - 1)
 
-
 print(factorial(100))
-
+'''
 
 '''
 print((lambda number, power : number ** power)(2,4))
