e2328136637273c4267149dabc8d26e5cff79729
zeitgeist7 <zeitgeist7@gmail.com>
2014-01-02 04:49:41 +0100

    Ordinary recursion
    Well, recursion
    Waiting for the fun part still
    
    #bored

--- playground/playground.py
+++ playground/playground.py
@@ -6,9 +6,19 @@ So far, Python seems a bit boring. But this tutorial seems promising.
 Do not expect to be amazed if you have opened this file by curiousity!!!
 '''
 
+def factorial(number) :
+	if number <= 1:
+		return 1
+	else :
+		return number * factorial(number - 1)
+
+
+print(factorial(100))
+
+
+'''
 print((lambda number, power : number ** power)(2,4))
-
-
+'''
 
 '''
 from funckyModule import *
