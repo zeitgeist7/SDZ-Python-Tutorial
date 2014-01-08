c100d0c5368ae695ae57df6e3a3860faceb8c29f
zeitgeist7 <zeitgeist7@gmail.com>
2014-01-08 11:38:15 +0100

    Just for fun
    Add new function for palindromes
    Use recursion for finding so exceeds the recursion depth for the longest palindrome know
        (Use iteration in that case)

--- playground/playground.py
+++ playground/playground.py
@@ -11,6 +11,39 @@ from Person import *
 zeitgeist7 = Person('rajiv', 'jhoomuck', 'male', 'Jayantee')
 print(zeitgeist7.parent)
 
+'''
+import string
+
+def removePunctuations(punctuatedStr):
+		punctuations = string.punctuation
+		for punctuation in punctuations:
+			punctuatedStr = punctuatedStr.replace(punctuation, "")
+		return punctuatedStr
+
+def palindromeHelper(str):
+	if str.__len__() <= 1:
+		return True
+	elif str[0] == str[-1]:
+		return isPalindrome(str[1:-1])
+	print(str)
+	return False
+
+def isPalindrome(str):
+	str = str.upper().replace(" ", "")
+	# str = [character for character in str if character not in string.punctuation]
+	str = removePunctuations(str)
+
+	return palindromeHelper(str)
+
+
+palindromes = ["civic", "Dewed", "deified", "dad", "mom", "devoved", "Hannah", "peeweep", "repaper", "kayak", "minim", "radar", "murdrum", "Malayalam", "madam", "lemel", "level", "racecar", "radar", "redder", "bob", "pop", "tot", "refer", "reviver", "rotator", "rotavator", "stats", "solos", "tenet", "terret", "testset", "Kinikinik", "Wassamassaw", "Yreka Bakery", "Navan", "Cain: a maniac.", "A Toyota.", "Race fast, safe car.", "A man, a plan, a canal: Panama.", "A dog, a plan, a canal: Pagoda.", "Desserts, I stressed!", "Drab as a fool, aloof as a bard.", "Live not on Evil.", "Madam, I'm Adam.", "Never odd or even.", "No lemon, no melon.", "Was it a car or a cat I saw?", "\"Not New York.\" Roy went on.", "Not so, Boston"]
+
+# test(s)
+for palindrome in palindromes:
+	print(isPalindrome(palindrome))
+
+'''
+
 
 
 '''
